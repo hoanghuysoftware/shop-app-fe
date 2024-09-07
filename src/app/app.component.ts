8b9babe65be58eda8acd485b2b6e30cc7c6d5182
hoanghuysoftware <hoanghuy.software@gmail.com>
2024-09-07 22:00:41 +0700

    Complete componet order,confirm,product,home in state basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { OrderComponent } from './order/order.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { HeaderComponent } from "./header/header.component";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet, RouterLink, HeaderComponent, FooterComponent, HomeComponent, OrderComponent, OrderConfirmComponent, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full', title: 'Hihi-Shop' },
+  { path: 'register', component: RegisterComponent, title: 'Đăng ký' },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.router').then((r) => r.LOGIN_ROUTES),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.router').then((b) => b.HOME_ROUTES),
+  },
+  {
+    path: 'order',
+    loadChildren: () =>
+      import('./order/order.router').then((cx) => cx.ORDER_ROUTES),
+  },
+];
